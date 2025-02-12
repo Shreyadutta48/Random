2b7898a6f097fe46e6d42ee67fdbbd9acdb1ec35
Shreyadutta48 <csrkl10bshreyadutta@gmail.com>
2025-02-12 02:25:40 +0000

    Variables.js

---
+++ .devcontainer/1_basics/Conversion.js
@@ -0,0 +1,20 @@
+let score = "33"
+console.log(typeof score)
+let valueScore = Number(score)
+console.log(typeof(valueScore))
+
+// "33" gets converted to 33(number)
+// "33abc" => NaN
+// true = 1,false = 0
+let isLoggedIn = 1;
+let boolValue = Boolean(isLoggedIn)
+console.log(isLoggedIn)
+console.log(boolValue)
+console.log(typeof(isLoggedIn))
+console.log(typeof(boolvalue))
+// for isLoggedIn = "", boolvalue = false
+// for isLoggedIn = "string", boolvalue = true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
