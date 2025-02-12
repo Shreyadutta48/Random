68c4d461c58ce29dc4b6f0c1895f95be777a89c5
Shreyadutta48 <csrkl10bshreyadutta@gmail.com>
2025-02-12 03:00:34 +0000

    Conversion.js

--- .devcontainer/1_basics/Conversion.js
+++ .devcontainer/1_basics/Conversion.js
@@ -1,22 +1,43 @@
 let score = "33"
-console.log(typeof score)
-let valueScore = Number(score)
-console.log(typeof(valueScore))
+//console.log(typeof score)
+//let valueScore = Number(score)
+//console.log(typeof(valueScore))
 
 // "33" gets converted to 33(number)
 // "33abc" => NaN
 // true = 1,false = 0
 let isLoggedIn = 1;
 let boolValue = Boolean(isLoggedIn)
-console.log(isLoggedIn)
-console.log(boolValue)
-console.log(typeof(isLoggedIn))
-console.log(typeof(boolvalue))
+//console.log(isLoggedIn)
+//console.log(boolValue)
+//console.log(typeof(isLoggedIn))
+//console.log(typeof(boolvalue))
 // for isLoggedIn = "", boolvalue = false
 // for isLoggedIn = "string", boolvalue = true
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+//console.log(stringNumber)
+//console.log(typeof(stringNumber))
 
 // ****Operations****
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = "Shreya"
+let str = str1 + str2
+console.log(str)
+
+//console.log("1" + 2)
+//console.log(1 + "2")
+//console.log("1" + 2 + 2)
+//console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter)
